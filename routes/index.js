9ee88dd3c9600f82960624279d4cc4502bcca0a7
Vishvesh Shah <vmshah@scu.edu>
2015-07-01 18:57:38 -0700

    Some changes
    trying

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/user', function (req, res, next) {
+  var sda = req.body.textinput;
+  console.log(sda);
+  next();
+});
+
+router.get('/', function(req, res) {
+  res.render('index');
 });
 
 module.exports = router;
