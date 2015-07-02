21ebce88f4b31f01b628e63eb055d3b3590cc970
Vishvesh Shah <vmshah@scu.edu>
2015-07-01 18:58:22 -0700

    removed routes
    not needed

--- app.js
+++ app.js
@@ -5,34 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var app = express();
 var AlchemyAPI = require('./alchemyapi');
 var alchemyapi = new AlchemyAPI();
-
-function entities() {
-  alchemyapi.AlchemyAPI();
-}
-
-var app = express();
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,14 +28,11 @@ app.use(function(req, res, next) {
 });
 
 // production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
   });
-});
+
+
 
 var server = app.listen(3000, function () {
 
