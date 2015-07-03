ff58e9a4c6bbe29bffed95ea5d79bbd7e5fef658
Vishvesh Shah <vmshah@scu.edu>
2015-07-02 18:20:05 -0700

    Fixed proper routing

--- alchemyapi.js
+++ alchemyapi.js
@@ -21,31 +21,12 @@ var fs = require('fs');
 
 //Make the class available
 exports = module.exports = AlchemyAPI;
-AlchemyAPI();
 
 function AlchemyAPI() {
 
-	//Load the key from api_key.txt
-	try {
-		key = fs.readFileSync(__dirname + '/api_key.txt').toString().trim();
-	}
-	catch(err) {
-		//Create the file
-		fs.openSync(__dirname + '/api_key.txt', 'w');
-		console.log('API key not detected in api_key.txt, please run: node alchemyapi.js YOUR_KEY_HERE');
-		console.log('If you do not have a key, register for one at: http://www.alchemyapi.com/api/register.html');
-		process.exit(1);
-	}
-	
-	//Make sure the key formating looks good
-	if (key.length != 40) {
-		console.log('The API key in api_key.txt does not appear to be valid. Make sure to run: node alchemyapi.js YOUR_KEY_HERE');
-		console.log('If you do not have a key, register for one at: http://www.alchemyapi.com/api/register.html');
-		process.exit(1);
-	}
 
 	//Set the key
-	this.apikey = key;
+	this.apikey = ''; //key here
 
 
 	/**
--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/');
+
 var app = express();
 var AlchemyAPI = require('./alchemyapi');
 var alchemyapi = new AlchemyAPI();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,19 +21,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+function restrict(req, res, next) {
+  if (req.body.textinput == "hello") {
+    //console.log("asdad");
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
+
+app.get('/', routes.index);
+
+app.post('/user',restrict, routes.user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Page Not Found');
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {message: err.message,error: err
+    });
+  });
+}
+
 // production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
+  res.render('error', {message: err.message,error: {}
   });
-
+});
 
 
 var server = app.listen(3000, function () {
